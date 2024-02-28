472a79306e3bee0fded002d5a7e89a26f9da6010
方阿森 <asen@fangasendeMac-mini.local>
2024-02-29 00:38:54 +0800

    ts-node change to node
    use js

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import { createPackage, deleteFilesRecursively, genConfigJson } from "./utils.js";
+import { genReact } from "./react.js";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const iconConfig = path.resolve(dirname, '../packages/svg/config.json');
+const iconTarget = path.resolve(dirname, '../packages/svg/icons');
+
+const iconCustomConfig = path.resolve(dirname, '../packages/svg/config.custom.json');
+const iconCustomTarget = path.resolve(dirname, '../packages/svg/__custom');
+
+deleteFilesRecursively(iconTarget);
+deleteFilesRecursively(iconCustomTarget);
+
+createPackage(iconTarget);
+createPackage(iconCustomTarget);
+
+genConfigJson(iconConfig, iconTarget)
+genConfigJson(iconCustomConfig, iconCustomTarget)
+
+genReact([iconTarget, iconCustomTarget]);
---
+++ scripts/react.js
@@ -0,0 +1,84 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { createPackage, deleteFilesRecursively, genCompFile, preprocessSvg } from './utils.js';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const getReactExportTemplate = (svg, name) => {
+  const propsHandle = svg.replace('<svg', '<svg\nstyle={ {fontSize: props.size, color: props.fill, rotate: props.rotate} }\n')
+
+  const temp =
+`const ${name} = (props: MasterIconProps) => {
+  return (
+    ${propsHandle}
+  );
+};
+  
+export default ${name};  
+`;
+
+return temp;
+}
+
+const genIndexFile = (path) => {
+  const temp =
+`
+import * as iconMap from './map';
+export { iconMap };
+
+export * from './map';
+export * from './types.d';
+`;
+  fs.writeFileSync(path, temp)
+}
+
+const genMappingFile = (
+  mappingFilePath,
+  svgSet
+) => {
+  const getMappingExportTemplate = (component) => {
+    const svgName = path.basename(component, '.svg');
+    const temp = `export { default as ${svgName} } from './icons/${svgName}.tsx';`
+    return temp;
+  }
+  
+  const mappingExportTemplate = Array.from(svgSet).map(getMappingExportTemplate).join('\n');
+
+  fs.writeFileSync(mappingFilePath, mappingExportTemplate);
+}
+
+export const genReact = (sourcesSVGs) => {
+  const reactConfig = {
+    targetComponents: path.resolve(dirname, '../icons'),
+    mapDirPath: path.resolve(dirname, '../map.ts'),
+    indexDirPath: path.resolve(dirname, '../index.ts')
+  }
+  
+  const { targetComponents, mapDirPath, indexDirPath } = reactConfig;
+  
+  [targetComponents, mapDirPath, indexDirPath].forEach(deleteFilesRecursively);
+
+  const iconsSet = new Set();
+  
+  for (const sourcesSVG of sourcesSVGs) {
+    fs.readdirSync(sourcesSVG).forEach((icon) => iconsSet.add(icon));
+
+    const packageDir = createPackage(targetComponents);
+    
+    genCompFile(sourcesSVG, packageDir, (sourcePath, targetDir) => {
+      const sourceFileName = path.basename(sourcePath, '.svg');
+      const targetPath = path.resolve(targetDir, `${sourceFileName}.tsx`);
+    
+      const sourceContent = preprocessSvg(fs.readFileSync(sourcePath, 'utf-8'));
+      const reactString = getReactExportTemplate(sourceContent, sourceFileName);
+    
+      fs.writeFileSync(targetPath, reactString);
+    })
+  }
+
+  genMappingFile(mapDirPath, iconsSet);
+  genIndexFile(indexDirPath)  
+}
