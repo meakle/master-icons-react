c12e69493c8c2379acc83096bce196b9bed59239
方阿森 <meakle@qq.com>
2024-02-29 18:51:58 +0800

    ci

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
     outDir: 'lib',
     lib: {
       entry: resolve(__dirname, './index.ts'),
-      name: 'lib',
-      fileName: 'lib',
+      name: 'icons',
+      fileName: 'icons',
       formats: ['cjs', 'es', 'umd'],
     },
     rollupOptions: {
